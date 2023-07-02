9456db4e7ea811f0bfad453aeb386228b3c5a90a
Алексей <com.twin@yandex.ru>
2023-07-02 20:42:44 +0700

    feature(ItemsTabs.cs): Added a UI.

---
+++ ObjectOrientedPractics/View/Tabs/ItemsTabs.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace ObjectOrientedPractics.View.Tabs
+{
+    partial class ItemsTabs
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ItemsTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.ItemsGroupBox = new System.Windows.Forms.GroupBox();
+            this.SelectedItemGroupBox = new System.Windows.Forms.GroupBox();
+            this.ItemsListBox = new System.Windows.Forms.ListBox();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.IdLabel = new System.Windows.Forms.Label();
+            this.CostLabel = new System.Windows.Forms.Label();
+            this.IdTextBox = new System.Windows.Forms.TextBox();
+            this.CostTextBox = new System.Windows.Forms.TextBox();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.NameRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.DescriptionLabel = new System.Windows.Forms.Label();
+            this.DescriptionRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.ItemsTableLayoutPanel.SuspendLayout();
+            this.ItemsGroupBox.SuspendLayout();
+            this.SelectedItemGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ItemsTableLayoutPanel
+            // 
+            this.ItemsTableLayoutPanel.ColumnCount = 2;
+            this.ItemsTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.ItemsTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.ItemsTableLayoutPanel.Controls.Add(this.ItemsGroupBox, 0, 0);
+            this.ItemsTableLayoutPanel.Controls.Add(this.SelectedItemGroupBox, 1, 0);
+            this.ItemsTableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ItemsTableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.ItemsTableLayoutPanel.Name = "ItemsTableLayoutPanel";
+            this.ItemsTableLayoutPanel.RowCount = 1;
+            this.ItemsTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.ItemsTableLayoutPanel.Size = new System.Drawing.Size(696, 445);
+            this.ItemsTableLayoutPanel.TabIndex = 0;
+            // 
+            // ItemsGroupBox
+            // 
+            this.ItemsGroupBox.Controls.Add(this.SaveButton);
+            this.ItemsGroupBox.Controls.Add(this.DeleteButton);
+            this.ItemsGroupBox.Controls.Add(this.AddButton);
+            this.ItemsGroupBox.Controls.Add(this.ItemsListBox);
+            this.ItemsGroupBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ItemsGroupBox.Location = new System.Drawing.Point(3, 3);
+            this.ItemsGroupBox.Name = "ItemsGroupBox";
+            this.ItemsGroupBox.Size = new System.Drawing.Size(342, 439);
+            this.ItemsGroupBox.TabIndex = 0;
+            this.ItemsGroupBox.TabStop = false;
+            this.ItemsGroupBox.Text = "Items";
+            // 
+            // SelectedItemGroupBox
+            // 
+            this.SelectedItemGroupBox.Controls.Add(this.DescriptionRichTextBox);
+            this.SelectedItemGroupBox.Controls.Add(this.DescriptionLabel);
+            this.SelectedItemGroupBox.Controls.Add(this.NameRichTextBox);
+            this.SelectedItemGroupBox.Controls.Add(this.NameLabel);
+            this.SelectedItemGroupBox.Controls.Add(this.CostTextBox);
+            this.SelectedItemGroupBox.Controls.Add(this.IdTextBox);
+            this.SelectedItemGroupBox.Controls.Add(this.CostLabel);
+            this.SelectedItemGroupBox.Controls.Add(this.IdLabel);
+            this.SelectedItemGroupBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SelectedItemGroupBox.Location = new System.Drawing.Point(351, 3);
+            this.SelectedItemGroupBox.Name = "SelectedItemGroupBox";
+            this.SelectedItemGroupBox.Size = new System.Drawing.Size(342, 439);
+            this.SelectedItemGroupBox.TabIndex = 1;
+            this.SelectedItemGroupBox.TabStop = false;
+            this.SelectedItemGroupBox.Text = "Selected Item";
+            // 
+            // ItemsListBox
+            // 
+            this.ItemsListBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.ItemsListBox.FormattingEnabled = true;
+            this.ItemsListBox.ItemHeight = 15;
+            this.ItemsListBox.Location = new System.Drawing.Point(6, 22);
+            this.ItemsListBox.Name = "ItemsListBox";
+            this.ItemsListBox.Size = new System.Drawing.Size(330, 364);
+            this.ItemsListBox.TabIndex = 0;
+            // 
+            // AddButton
+            // 
+            this.AddButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.AddButton.Location = new System.Drawing.Point(6, 392);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(106, 41);
+            this.AddButton.TabIndex = 1;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.DeleteButton.Location = new System.Drawing.Point(118, 392);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(106, 41);
+            this.DeleteButton.TabIndex = 2;
+            this.DeleteButton.Text = "Delete";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            // 
+            // IdLabel
+            // 
+            this.IdLabel.AutoSize = true;
+            this.IdLabel.Location = new System.Drawing.Point(6, 57);
+            this.IdLabel.Name = "IdLabel";
+            this.IdLabel.Size = new System.Drawing.Size(21, 15);
+            this.IdLabel.TabIndex = 0;
+            this.IdLabel.Text = "ID:";
+            // 
+            // CostLabel
+            // 
+            this.CostLabel.AutoSize = true;
+            this.CostLabel.Location = new System.Drawing.Point(6, 86);
+            this.CostLabel.Name = "CostLabel";
+            this.CostLabel.Size = new System.Drawing.Size(34, 15);
+            this.CostLabel.TabIndex = 1;
+            this.CostLabel.Text = "Cost:";
+            // 
+            // IdTextBox
+            // 
+            this.IdTextBox.Location = new System.Drawing.Point(48, 54);
+            this.IdTextBox.Name = "IdTextBox";
+            this.IdTextBox.Size = new System.Drawing.Size(100, 23);
+            this.IdTextBox.TabIndex = 2;
+            // 
+            // CostTextBox
+            // 
+            this.CostTextBox.Location = new System.Drawing.Point(48, 83);
+            this.CostTextBox.Name = "CostTextBox";
+            this.CostTextBox.Size = new System.Drawing.Size(100, 23);
+            this.CostTextBox.TabIndex = 3;
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Location = new System.Drawing.Point(6, 115);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(42, 15);
+            this.NameLabel.TabIndex = 4;
+            this.NameLabel.Text = "Name:";
+            // 
+            // NameRichTextBox
+            // 
+            this.NameRichTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.NameRichTextBox.Location = new System.Drawing.Point(6, 133);
+            this.NameRichTextBox.Name = "NameRichTextBox";
+            this.NameRichTextBox.Size = new System.Drawing.Size(330, 96);
+            this.NameRichTextBox.TabIndex = 6;
+            this.NameRichTextBox.Text = "";
+            // 
+            // DescriptionLabel
+            // 
+            this.DescriptionLabel.AutoSize = true;
+            this.DescriptionLabel.Location = new System.Drawing.Point(6, 232);
+            this.DescriptionLabel.Name = "DescriptionLabel";
+            this.DescriptionLabel.Size = new System.Drawing.Size(70, 15);
+            this.DescriptionLabel.TabIndex = 7;
+            this.DescriptionLabel.Text = "Description:";
+            // 
+            // DescriptionRichTextBox
+            // 
+            this.DescriptionRichTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DescriptionRichTextBox.Location = new System.Drawing.Point(6, 250);
+            this.DescriptionRichTextBox.Name = "DescriptionRichTextBox";
+            this.DescriptionRichTextBox.Size = new System.Drawing.Size(330, 96);
+            this.DescriptionRichTextBox.TabIndex = 8;
+            this.DescriptionRichTextBox.Text = "";
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.SaveButton.Location = new System.Drawing.Point(230, 392);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(106, 41);
+            this.SaveButton.TabIndex = 3;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            // 
+            // ItemsTabs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.ItemsTableLayoutPanel);
+            this.MinimumSize = new System.Drawing.Size(696, 445);
+            this.Name = "ItemsTabs";
+            this.Size = new System.Drawing.Size(696, 445);
+            this.ItemsTableLayoutPanel.ResumeLayout(false);
+            this.ItemsGroupBox.ResumeLayout(false);
+            this.SelectedItemGroupBox.ResumeLayout(false);
+            this.SelectedItemGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TableLayoutPanel ItemsTableLayoutPanel;
+        private GroupBox ItemsGroupBox;
+        private GroupBox SelectedItemGroupBox;
+        private ListBox ItemsListBox;
+        private Button DeleteButton;
+        private Button AddButton;
+        private TextBox CostTextBox;
+        private TextBox IdTextBox;
+        private Label CostLabel;
+        private Label IdLabel;
+        private Label NameLabel;
+        private RichTextBox DescriptionRichTextBox;
+        private Label DescriptionLabel;
+        private RichTextBox NameRichTextBox;
+        private Button SaveButton;
+    }
+}
---
+++ ObjectOrientedPractics/View/Tabs/ItemsTabs.cs
@@ -0,0 +1,13 @@
+﻿namespace ObjectOrientedPractics.View.Tabs
+{
+    /// <summary>
+    /// Класс, описывающий пользовательский элемент управления, содержащий ItemsGroupBox.
+    /// </summary>
+    public partial class ItemsTabs : UserControl
+    {
+        public ItemsTabs()
+        {
+            InitializeComponent();
+        }
+    }
+}
