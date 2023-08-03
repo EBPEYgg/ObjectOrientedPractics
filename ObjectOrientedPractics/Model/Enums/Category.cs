2d4440b279dfe04a867c0f244ded5db2ed35f8d6
Alexey <com.twin@yandex.ru>
2023-08-03 21:25:26 +0700

    feature(Category.cs): added enumeration with product categories.

---
+++ ObjectOrientedPractics/Model/Enums/Category.cs
@@ -0,0 +1,40 @@
+﻿/// <summary>
+/// Перечисление, описывающее категории товаров.
+/// </summary>
+public enum Category
+{
+    /// <summary>
+    /// Комплектующие для ПК.
+    /// </summary>
+    PCcomponents,
+
+    /// <summary>
+    /// Телефоны.
+    /// </summary>
+    Phones,
+
+    /// <summary>
+    /// Бытовая техника.
+    /// </summary>
+    DomesticAppliances,
+
+    /// <summary>
+    /// Строительство.
+    /// </summary>
+    Building,
+
+    /// <summary>
+    /// Мебель.
+    /// </summary>
+    Furniture,
+
+    /// <summary>
+    /// Товары для отдыха.
+    /// </summary>
+    Entertainments,
+
+    /// <summary>
+    /// Автомобильные товары.
+    /// </summary>
+    CarGoods
+}
