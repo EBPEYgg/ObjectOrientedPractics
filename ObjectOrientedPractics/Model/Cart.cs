13fece50ea6480f61e3651c7e297e848ff63ea42
Алексей <com.twin@yandex.ru>
2023-10-03 22:30:40 +0700

    feature(Cart.cs): A new class describing the customer's shopping cart.

---
+++ ObjectOrientedPractics/Model/Cart.cs
@@ -0,0 +1,47 @@
+﻿namespace ObjectOrientedPractics.Model
+{
+    public class Cart
+    {
+        /// <summary>
+        /// Список товаров.
+        /// </summary>
+        private List<Item> _items;
+
+        /// <summary>
+        /// Общая стоимость товаров.
+        /// </summary>
+        private int _amount;
+
+        /// <summary>
+        /// Возвращает и задает список товаров.
+        /// </summary>
+        public List<Item> Items
+        {
+            get => _items;
+            set => _items = value;
+        }
+
+        /// <summary>
+        /// Возвращает и задает общую стоимость товаров.
+        /// </summary>
+        public int Amount
+        {
+            get
+            {
+                if (_items == null || _items.Count == 0)
+                {
+                    return 0;
+                }
+                for (int i = 0; i < _items.Count; i++)
+                {
+                    _amount = _amount + _items[i].Cost;
+                }
+                return _amount;
+            }
+        }
+
+        public Cart()
+        {
+        }
+    }
+}
