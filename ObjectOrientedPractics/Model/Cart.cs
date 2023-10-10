d72a941c3e7d2e51fff98c2b35bf241cf888c751
Алексей <com.twin@yandex.ru>
2023-10-10 15:14:37 +0700

    fix(Cart.cs): Fixed the Amount property.

--- ObjectOrientedPractics/Model/Cart.cs
+++ ObjectOrientedPractics/Model/Cart.cs
@@ -31,13 +31,13 @@
         {
             get
             {
-                if (_items == null || _items.Count == 0)
+                if (Items == null || Items.Count == 0)
                 {
                     return 0;
                 }
-                for (int i = 0; i < _items.Count; i++)
+                for (int i = 0; i < Items.Count; i++)
                 {
-                    _amount = _amount + _items[i].Cost;
+                    _amount += Items[i].Cost;
                 }
                 return _amount;
             }
