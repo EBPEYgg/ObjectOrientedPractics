725d0b77be84d0983b81062c3d63975f6ed43814
Алексей <com.twin@yandex.ru>
2023-10-29 22:23:48 +0700

    feature(IDiscount.cs): Added an interface describing discounts.

---
+++ ObjectOrientedPractics/Model/IDiscount.cs
@@ -0,0 +1,28 @@
+﻿namespace ObjectOrientedPractics.Model
+{
+    /// <summary>
+    /// Интерфейс, описывающий скидки.
+    /// </summary>
+    public interface IDiscount
+    {
+        /// <summary>
+        /// Метод, который считает размер скидки.
+        /// </summary>
+        /// <param name="items">Список товаров.</param>
+        /// <returns>Размер скидки.</returns>
+        double Calculate(List<Item> items);
+
+        /// <summary>
+        /// Метод, который применяет скидку к товарам.
+        /// </summary>
+        /// <param name="items">Список товаров.</param>
+        /// <returns>Размер скидки с вычетом накопленных баллов.</returns>
+        double Apply(List<Item> items);
+
+        /// <summary>
+        /// Метод, который накапливает скидку или списывает баллы.
+        /// </summary>
+        /// <param name="items">Список товаров.</param>
+        void Update(List<Item> items);
+    }
+}
