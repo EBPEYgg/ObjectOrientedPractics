e9026bdc6a1dc369bd47dafbcb6918fe3c41a64c
Алексей <com.twin@yandex.ru>
2023-10-31 22:57:43 +0700

    feature(AddDiscountForm.cs): Added a form for adding a new percentage discount.

---
+++ ObjectOrientedPractics/View/Forms/AddDiscountForm.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace ObjectOrientedPractics.View.Forms
+{
+    partial class AddDiscountForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OkButton = new System.Windows.Forms.Button();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.PercentDiscountLabel = new System.Windows.Forms.Label();
+            this.CategoryLabel = new System.Windows.Forms.Label();
+            this.CategoryComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // OkButton
+            // 
+            this.OkButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.OkButton.Location = new System.Drawing.Point(133, 60);
+            this.OkButton.Name = "OkButton";
+            this.OkButton.Size = new System.Drawing.Size(88, 34);
+            this.OkButton.TabIndex = 0;
+            this.OkButton.Text = "Ok";
+            this.OkButton.UseVisualStyleBackColor = true;
+            this.OkButton.Click += new System.EventHandler(this.OkButton_Click);
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.CancelButton.Location = new System.Drawing.Point(227, 60);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(88, 34);
+            this.CancelButton.TabIndex = 1;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // PercentDiscountLabel
+            // 
+            this.PercentDiscountLabel.AutoSize = true;
+            this.PercentDiscountLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.PercentDiscountLabel.Location = new System.Drawing.Point(12, 9);
+            this.PercentDiscountLabel.Name = "PercentDiscountLabel";
+            this.PercentDiscountLabel.Size = new System.Drawing.Size(106, 15);
+            this.PercentDiscountLabel.TabIndex = 2;
+            this.PercentDiscountLabel.Text = "Percent Discount:";
+            // 
+            // CategoryLabel
+            // 
+            this.CategoryLabel.AutoSize = true;
+            this.CategoryLabel.Location = new System.Drawing.Point(12, 34);
+            this.CategoryLabel.Name = "CategoryLabel";
+            this.CategoryLabel.Size = new System.Drawing.Size(58, 15);
+            this.CategoryLabel.TabIndex = 3;
+            this.CategoryLabel.Text = "Category:";
+            // 
+            // CategoryComboBox
+            // 
+            this.CategoryComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CategoryComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CategoryComboBox.FormattingEnabled = true;
+            this.CategoryComboBox.Location = new System.Drawing.Point(76, 31);
+            this.CategoryComboBox.Name = "CategoryComboBox";
+            this.CategoryComboBox.Size = new System.Drawing.Size(239, 23);
+            this.CategoryComboBox.TabIndex = 4;
+            // 
+            // AddDiscountForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 106);
+            this.Controls.Add(this.CategoryComboBox);
+            this.Controls.Add(this.CategoryLabel);
+            this.Controls.Add(this.PercentDiscountLabel);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.OkButton);
+            this.MaximumSize = new System.Drawing.Size(350, 145);
+            this.MinimumSize = new System.Drawing.Size(350, 145);
+            this.Name = "AddDiscountForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Add Discount";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button OkButton;
+        private Button CancelButton;
+        private Label PercentDiscountLabel;
+        private Label CategoryLabel;
+        private ComboBox CategoryComboBox;
+    }
+}
---
+++ ObjectOrientedPractics/View/Forms/AddDiscountForm.cs
@@ -0,0 +1,41 @@
+﻿using ObjectOrientedPractics.Model;
+using static ObjectOrientedPractics.Model.Discounts.PercentDiscount;
+
+namespace ObjectOrientedPractics.View.Forms
+{
+    public partial class AddDiscountForm : Form
+    {
+        /// <summary>
+        /// Возвращает и задает выбранный индекс.
+        /// </summary>
+        public int SelectedIndex { get; set; }
+
+        /// <summary>
+        /// Возвращает и задает список покупателей.
+        /// </summary>
+        public List<Customer> Customers { get; set; }
+
+        public AddDiscountForm()
+        {
+            InitializeComponent();
+            CategoryComboBox.DataSource = Enum.GetValues(typeof(Category));
+            CategoryComboBox.SelectedIndex = -1;
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void OkButton_Click(object sender, EventArgs e)
+        {
+            if (CategoryComboBox.SelectedIndex != -1)
+            {
+                PercentDiscounts percentDiscount = new PercentDiscounts();
+                percentDiscount.Category = (Category)CategoryComboBox.SelectedItem;
+                Customers[SelectedIndex].Discounts.Add(percentDiscount);
+                Close();
+            }
+        }
+    }
+}
